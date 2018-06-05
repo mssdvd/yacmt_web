ee6be8b7ca01e3749bd0404b3427982a60990bee
Davide Masserut <d.masserut@gmail.com>
2018-06-05 21:28:04 +0200

    Initial release

---
+++ app.py
@@ -0,0 +1,30 @@
+import json
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table_experiments as dt
+import pandas
+import plotly
+from dash.dependencies import Input, Output, State
+from sqlalchemy import create_engine
+
+app = dash.Dash()
+engine = create_engine(
+    "***REMOVED***"
+)
+
+DATA = pandas.read_sql("SELECT * FROM reports", engine)
+
+app.layout = html.Div([
+    html.H4('Records'),
+    dt.DataTable(
+        rows=DATA.to_dict('records'),
+        columns=["id", "veicolo"],
+        filterable=True,
+        sortable=True,
+        selected_row_indices=[])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
